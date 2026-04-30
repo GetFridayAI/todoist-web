a87a28c9b6c1cc8db3de0eda75c764d9d132639b
Amit Rai <amit.rai1095@gmail.com>
2026-04-30 18:04:25 +0530

    Remove unused useTheme import
    Removed unused useTheme import from ThemeContext.
    
    Signed-off-by: Amit Rai <amit.rai1095@gmail.com>

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import Dashboard from './src/webos/components/dashboard/Dashboard';
 import { DashboardRoutes as AppRoutes } from './src/shared/interfaces/routes.interface';
 import { View } from 'react-native';
 import { APP_THEMES } from './src/shared/interfaces/app.interface';
-import { ThemeProvider, useTheme } from './src/shared/context/ThemeContext';
+import { ThemeProvider } from './src/shared/context/ThemeContext';
 import appStyles from './src/shared/styles/app.styles';
 
 const ROUTE_SEGMENT_MAP: Record<string, AppRoutes> = {
@@ -39,7 +39,6 @@ function DashboardRoute() {
 const APP_THEME = APP_THEMES.DARK;
 
 function ThemedApp() {
-  const { styles } = useTheme();
   return (
     <View style={[appStyles[APP_THEME], { flex: 1 }]}>
       <BrowserRouter>
